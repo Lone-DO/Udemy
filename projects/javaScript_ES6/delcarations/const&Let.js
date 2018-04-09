b4361fca171073afe46f227f6da5a4646a1894c7
lone-DO <Lone-DO@users.noreply.github.com>
2018-04-09 06:36:47 -0400

    understanding Let & Const

---
+++ projects/javaScript_ES6/delcarations/const&Let.js
@@ -0,0 +1,18 @@
+/**
+ * var name: 'Jane';
+ * var title: 'Software Enginerr';
+ * var hourlyWage: 40;
+ */
+
+//ES6
+//const// Constant declartion for permanent values || Read-only
+//let// Let declarations for values be flexible || Read & Write
+
+const name = "Jane";
+let title = "Software Engineer";
+let hourlyWage = 40;
+
+// some time later
+
+title = "Sr. Software Engineer";
+hourlyWage = 45;
