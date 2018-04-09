6b33f43e6e8f390293ae2ce8fc8a48d9b13f8833
lone-DO <Lone-DO@users.noreply.github.com>
2018-04-09 06:56:21 -0400

    Declaring Const & Let

--- projects/javaScript_ES6/delcarations/const&Let.js
+++ projects/javaScript_ES6/delcarations/const&Let.js
@@ -16,3 +16,55 @@ let hourlyWage = 40;
 
 title = "Sr. Software Engineer";
 hourlyWage = 45;
+
+//CHALLENGES BELOW
+
+/**
+ * Imagine that you are building an application to manage a user's Facebook profile.
+ *    A profile might have a 'name', 'age', 'dateOfBirth'.
+ * Declare three variables with these same name, making sure to use 'const' or 'let'
+ *    depending on whether you expect the value to change over time.
+ */
+
+const name = "billy";
+let age = 25;
+const dateOfBirth = "01/01/90";
+
+// ---------
+/**
+ * * The following code uses 'var' instead of const and 'let'.
+*   Refactor the function to use the new keywords.
+* Be sure to consider whether the variable shoud be declared using 'const' or 'let' 
+*   depending on whether the variable gets reassigned or not.
+
+  var statuses = [
+  { code: 'OK', response: 'Request successful' },
+  { code: 'FAILED', response: 'There was an error with your request' },
+  { code: 'PENDING', response: 'Your reqeust is still pending' }
+];
+var message = '';
+var currentCode = 'OK';
+
+for (var i = 0; i < statuses.length; i++) {
+  if (statuses[i].code === currentCode) {
+    message = statuses[i].response;
+  }
+}
+*
+ */
+
+const statuses = [
+   { code: "OK", response: "Request successful" },
+   { code: "FAILED", response: "There was an error with your request" },
+   { code: "PENDING", response: "Your reqeust is still pending" }
+];
+let message = "";
+const currentCode = "OK";
+
+for (var i = 0; i < statuses.length; i++) {
+   if (statuses[i].code === currentCode) {
+      message = statuses[i].response;
+   }
+}
+
+// -------
