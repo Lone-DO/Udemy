592022081d72d721532e92afa6dd97550d9266dd
lone-DO <lotussythe@gmail.com>
2018-05-08 20:06:05 -0400

    understanding Generators w/ for of loops

---
+++ projects/javaScript_ES6/delcarations/generators.js
@@ -0,0 +1,63 @@
+/**CONVULTED EXAMPLE OF GENERATORS */
+
+/** 
+function* shopping() {
+   //stuff on the sidewalk
+
+   //walking down the sidewalk
+
+   //go into the store with cash
+   const stuffFromStore = yield 'cash';
+   // walking to laundry place
+   const cleanClothes = yield 'laundry';
+   //walking back home
+   return [stuffFromStore, cleanClothes];
+}
+
+//stuff in the store
+const gen = shopping();
+gen.next(); //leaving our house
+//walked into the store
+//walking up and down the aisles...
+//purchase our stuff
+
+gen.next('groceries'); //leaving the store with groceries
+gen.next('cleanclothes');
+
+*/
+/**END OF CONVULTED EXPRESSION */
+
+function* colors() {
+   yield "red";
+   yield "blue";
+   yield "green";
+}
+
+const myColors = [];
+
+for (let color of colors()) {
+   myColors.push(color);
+}
+
+//myColors;
+
+/******* */
+
+const engineeringTeam = {
+   size: 3,
+   department: "Engineering",
+   lead: "Jill",
+   manager: "Alex",
+   engineer: "Dave"
+};
+
+function* TeamIterator(team) {
+   yield team.lead;
+   yield team.manager;
+   yield team.engineer;
+}
+
+const names = [];
+for (let name of TeamIterator(engineeringTeam)) {
+   names.push(name);
+}
